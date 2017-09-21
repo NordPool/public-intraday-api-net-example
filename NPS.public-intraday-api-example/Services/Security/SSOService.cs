1a5a064bce5866389ae3017c147ff414cdcb46f2
Moilanen Risto <risto.moilanen@solteq.com>
2017-09-21 16:55:21 +0300

    Copyright clauses added to console example app.

--- NPS.public-intraday-api-example/Services/Security/SSOService.cs
+++ NPS.public-intraday-api-example/Services/Security/SSOService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ *  Copyright 2017 Nord Pool.
+ *  This library is intended to aid integration with Nord Pool’s Intraday API and comes without any warranty. Users of this library are responsible for separately testing and ensuring that it works according to their own standards.
+ *  Please send feedback to idapi@nordpoolgroup.com.
+ */
+
+using System;
 using Newtonsoft.Json;
 using NPS.public_intraday_api_example.Services.Security.Exceptions;
 using NPS.public_intraday_api_example.Utilities;
